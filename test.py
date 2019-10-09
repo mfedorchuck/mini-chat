d931479b8535f51bf15b9bdcf481a93aec48beb1
Maksim Fedorchuk <maksim.fedorchuk@wikrgroup.com>
2019-10-09 23:18:24 +0300

    test file

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-
-Form, Window = uic.loadUiType("dialog.ui")
-
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec_()
-
-
 class Client:
     def __init__(self,  age: int, login: str):
         self.age = age
